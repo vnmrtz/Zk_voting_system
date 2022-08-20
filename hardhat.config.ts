f25e775c645779355689de3d426687032b560855
Victor Martinez <vicroc.vn@gmail.com>
2022-08-20 02:49:20 +0200

    deploy_test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,28 +1,11 @@
+import { HardhatUserConfig } from "hardhat/config";
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require('@openzeppelin/hardhat-upgrades');
 
-module.exports = {
-  defaultNetwork: "matic",
-  networks: {
-    hardhat: {
-    },
-    matic: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  },
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY
-  },
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-}
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+};
+
+export default config;
